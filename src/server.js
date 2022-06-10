59ec0089cc8fe8a9e3dd4e7fde3bfee77d9a1893
Phan Van Phu <nostarwhere.pc@gmail.com>
2022-06-10 22:30:09 +0700

    change host

--- src/server.js
+++ src/server.js
@@ -26,6 +26,12 @@ initWebRoutes(app);
 
 connectDB();
 
-let port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server running on port : ${port}`)
+})
 
-app.listen(port);
+app.get('/', (req, res) => {
+  res.send('This is an server')
+})
+
+let port = process.env.PORT || 8080;
